252e015214b05ad881a48534862b7ebcfa57bd77
Gronnmann <gronnmannthecoder@gmail.com>
2016-06-18 14:48:29 +0200

    Updated plugin.yml and packets

---
+++ GameCore/src/io/github/gronnmann/gamecore/events/GameEndEvent.java
@@ -0,0 +1,62 @@
+package io.github.gronnmann.gamecore.events;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import io.github.gronnmann.gamecore.GameCore.StopReason;
+
+public class GameEndEvent extends Event{
+	private static HandlerList handler = new HandlerList();
+	
+	public static HandlerList getHandlerList(){
+		return handler;
+	}
+	
+	public HandlerList getHandlers(){
+		return handler;
+	}
+	private StopReason rsn;
+	private List<Player> winners, losers;
+	private boolean announceEvent;
+	
+	public GameEndEvent(StopReason reason, List<Player> winners, List<Player> losers, boolean announceEvent){
+		this.rsn = reason;
+		this.winners = winners;
+		this.losers = losers;
+		this.announceEvent = announceEvent;
+	}
+	
+	public GameEndEvent(StopReason reason, List<Player> winners, List<Player> losers){
+		this.rsn = reason;
+		this.winners = winners;
+		this.losers = losers;
+	}
+	
+	public GameEndEvent(StopReason reason){
+		this.rsn = reason;
+		this.winners = null;
+		this.losers = null;
+	}
+	
+	public GameEndEvent(StopReason reason, List<Player> winners){
+		this.rsn = reason;
+		this.winners = winners;
+		this.losers = null;
+	}
+	
+	public StopReason getReason(){
+		return rsn;
+	}
+	public List<Player> getWinners(){
+		return winners;
+	}
+	public List<Player> getLosers(){
+		return losers;
+	}
+	public boolean isAnnounced(){
+		return announceEvent;
+	}
+}
